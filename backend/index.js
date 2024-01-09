267413485277cf28eea8b33d1754ae935349adfa
Harsh Tiwari <131223935+harsh21082004@users.noreply.github.com>
2024-01-09 19:38:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,13 @@ const AttendanceModel = require('./models/AttendanceData'); // Import attendance
 const axios = require('axios')
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://mern-stack-murex.vercel.app"],
+    methods:["POST","GET"},
+    credentials:true
+  }
+));
 
 mongoose.connect('mongodb://127.0.0.1:27017/practice_mern');
 
